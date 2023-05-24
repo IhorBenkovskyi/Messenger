384c41da7534bdba5afac63cc747f6f74e66220a
IhorBenkovskyi <igorbenk2003@gmail.com>
2023-05-25 00:30:31 +0300

    created appContext and hide available loops

--- messenger-frontend/src/App.js
+++ messenger-frontend/src/App.js
@@ -5,18 +5,36 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Chat from './pages/Chat';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { AppContext, socket } from './context/appContext';
 
 const App = () => {
+
+  const [rooms, setRooms] = useState([]);
+  const [currentRoom, setCurrentRoom] = useState([]);
+  const [members, setMembers] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [privatMemberMsg, setPrivatMemberMsg] = useState({});
+  const [newMessages, setNewMessages] = useState({});
+
+  const user = useSelector(state => state.user);
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
+    <AppContext.Provider value={{ socket, currentRoom, setCurrentRoom, members, setMembers, messages, setMessages, privatMemberMsg, setPrivatMemberMsg, rooms, setRooms, newMessages, setNewMessages }}>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {!user && (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+            </>
+          )}
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
--- messenger-frontend/src/components/MessageForm.jsx
+++ messenger-frontend/src/components/MessageForm.jsx
@@ -3,25 +3,29 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './MessageForm.css';
+import { useSelector } from 'react-redux';
 
 const MessageForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
     }
+
+    const user = useSelector(state => state.user);
+
     return (
         <div>
-            <div className="messages-output"></div>
+            <div className="messages-output">{!user && <div className='alert alert-danger' >Please login</div>}</div>
             <Form onSubmit={handleSubmit}>
                 <Row>
                     <Col md={11}>
                         <Form.Group>
-                            <Form.Control type="text" placeholder='Your message'>
+                            <Form.Control type="text" placeholder='Your message' disabled={!user}>
                             </Form.Control>
                         </Form.Group>
                     </Col>
                     <Col md={1}>
-                        <Button type='submit' variant='primary' style={{ width: '100%', backgroundColor: 'orange' }}>
+                        <Button type='submit' variant='primary' style={{ width: '100%', backgroundColor: 'orange' }} disabled={!user}>
                             <i className='fas fa-paper-plane'></i>
                         </Button>
                     </Col>
