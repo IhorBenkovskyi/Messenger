8a16a179995635a432404c7e84f617e801439563
IhorBenkovskyi <igorbenk2003@gmail.com>
2023-05-25 18:19:16 +0300

    created logic for messageForm and  Sidebar

--- messenger-frontend/src/components/Sidebar.jsx
+++ messenger-frontend/src/components/Sidebar.jsx
@@ -1,12 +1,31 @@
 import React, { useContext } from 'react';
-import { ListGroup } from 'react-bootstrap';
+import { ListGroup, Col, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { AppContext } from '../context/appContext';
 import { useEffect } from 'react';
+import { addNotifications, resetNotifications } from "../features/userSlice";
+import { useDispatch } from 'react-redux';
+import './Sidebar.css';
 
 const Sidebar = () => {
     const user = useSelector(state => state.user);
-    const { socket, setMembers, members, setCurrentRoom, setRooms, privateMemberMsg, rooms, setPrivateMemberMsg, currentRoom } = useContext(AppContext);
+    const dispatch = useDispatch();
+    const { socket, setMembers, members, setCurrentRoom, setRooms, privateMemberMsg, rooms, setPrivatMemberMsg, currentRoom } = useContext(AppContext);
+
+
+    const joinRoom = (room, isPublic = true) => {
+        if (!user) {
+            return alert("Please login");
+        }
+        socket.emit("join-room", room, currentRoom);
+        setCurrentRoom(room);
+
+        if (isPublic) {
+            setPrivatMemberMsg(null);
+        }
+        // dispatch for notifications
+        dispatch(resetNotifications(room));
+    };
 
     useEffect(() => {
         if (user) {
@@ -29,17 +48,60 @@ const Sidebar = () => {
             .then((data) => setRooms(data));
     }
 
+    function orderIds(id1, id2) {
+        if (id1 > id2) {
+            return id1 + "-" + id2;
+        } else {
+            return id2 + "-" + id1;
+        }
+    }
+
+    function handlePrivateMemberMsg(member) {
+        setPrivatMemberMsg(member);
+        const roomId = orderIds(user._id, member._id);
+        joinRoom(roomId, false);
+    }
+
 
     if (!user) return <></>
     return <>
+        {/* <h2>Available rooms</h2> */}
+        {/* <ListGroup>
+            {rooms.map((room, idx) => (<ListGroup.Item key={idx} onClick={() => joinRoom()}>{room}</ListGroup.Item>))}
+        </ListGroup> */}
         <h2>Available rooms</h2>
         <ListGroup>
-            {rooms.map((room, idx) => (<ListGroup.Item key={idx}>{room}</ListGroup.Item>))}
+            {rooms.map((room, idx) => (
+                <ListGroup.Item key={idx} onClick={() => joinRoom(room)} active={room == currentRoom} style={{ cursor: "pointer", display: "flex", justifyContent: "space-between" }}>
+                    {room} {currentRoom !== room && <span className="badge rounded-pill bg-primary">{user.newMessages[room]}</span>}
+                </ListGroup.Item>
+            ))}
         </ListGroup>
         <h2>Members</h2>
-        {members.map((member, idx) => (<ListGroup.Item key={member.id} style={{ cursor: "pointer" }}>
+        {/* {members.map((member, idx) => (<ListGroup.Item key={member.id} style={{ cursor: "pointer" }}>
             {member.name}
-        </ListGroup.Item>))}
+        </ListGroup.Item>))} */}
+        <ListGroup>
+            {members.map((member) => (
+                <ListGroup.Item key={member.id} style={{ cursor: "pointer" }} active={privateMemberMsg?._id == member?._id} onClick={() => handlePrivateMemberMsg(member)} disabled={member._id === user._id}>
+                    <Row>
+                        <Col xs={2} className="member-status">
+                            <img src={member.picture} className="member-status-img" />
+                            {member.status == "online" ? <i className="fas fa-circle sidebar-online-status"></i> : <i className="fas fa-circle sidebar-offline-status"></i>}
+                        </Col>
+                        <Col xs={9}>
+                            {member.name}
+                            {member._id === user?._id && " (You)"}
+                            {member.status == "offline" && " (Offline)"}
+                        </Col>
+                        <Col xs={1}>
+                            <span className="badge rounded-pill bg-primary">{user.newMessages[orderIds(member._id, user._id)]}</span>
+                        </Col>
+                    </Row>
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+
     </>
 }
 
